6f5d6e94d3ae8b68a38e14dce30f7f38e52e3462
Garvit Singh <garvitsingh4575@gmail.com>
2021-02-18 18:31:34 +0400

    Stroke Changes 🥢

--- boundary.js
+++ boundary.js
@@ -1,14 +1,15 @@
 class Boundary {
-	constructor(x1, y1, x2, y2, c = color(255, 255, 255)) {
+	constructor(x1, y1, x2, y2, c = color(255, 255, 255), w = 5) {
 		this.a = createVector(x1, y1);
 		this.b = createVector(x2, y2);
 		this.c = c;
+		this.w = w;
 	}
 
 	show() {
 		stroke(this.c);
 
-		strokeWeight(rendering ? 3 : 5);
+		strokeWeight(rendering ? 3 : this.w);
 		line(this.a.x, this.a.y, this.b.x, this.b.y);
 	}
 }
